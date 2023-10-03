895577eba355024c7bcb99bcf82da6e391b8f74a
Daizy11 <habib.erdian@gmail.com>
2023-10-03 20:18:24 +0700

    new update

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -20,7 +20,7 @@ exports.getAllUser = async (req, res, next) => {
 exports.Register = async (req, res) => {
   try {
     const hashPassword = await bcrypt.hash(req.body.password, 10);
-    console.log(hash)
+    console.log(hashPassword)
     const user = await prisma.user.create({
       data: {
         name: req.body.name,
