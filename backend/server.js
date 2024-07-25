dd27385e55b705034be8b81251687059f71f951f
Habib <habib@192.168.1.18>
2024-07-25 17:22:25 +0700

    change second parameter of app.listen function into 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const app = require('./app')
-const port = process.env.port || 3001
+const port = process.env.port || 3000
 const env = require('dotenv')
 const kill = require('kill-port')
 
 env.config({ path: './.env' })
 
 
- app.listen(port, "0.0.0.0");
+ app.listen(port,"0.0.0.0", () => {
+    console.log(`App running on port ${port}`);
+   
+  });
